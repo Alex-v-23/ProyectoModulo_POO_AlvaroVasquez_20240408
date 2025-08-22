62715fa048ba0554643d8a476355368e3b6b35dd
Alex-v-23 <20240408@ricaldone.edu.sv>
2025-08-22 12:09:08 -0600

    initial commit

---
+++ src/main/java/AlvaroAlexanderVasquezCortez_20240408/AlvaroAlexanderVasquezCortez_20240408demo/AlvaroAlexanderVasquezCortez20240408demoApplication.java
@@ -0,0 +1,13 @@
+package AlvaroAlexanderVasquezCortez_20240408.AlvaroAlexanderVasquezCortez_20240408demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlvaroAlexanderVasquezCortez20240408demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlvaroAlexanderVasquezCortez20240408demoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AlvaroAlexanderVasquezCortez_20240408/AlvaroAlexanderVasquezCortez_20240408demo/AlvaroAlexanderVasquezCortez20240408demoApplicationTests.java
@@ -0,0 +1,13 @@
+package AlvaroAlexanderVasquezCortez_20240408.AlvaroAlexanderVasquezCortez_20240408demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlvaroAlexanderVasquezCortez20240408demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
